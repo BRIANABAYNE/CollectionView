1708909459fe5254a1337c0dc36921807c73bb7e
Briana Bayne <brianabayne1@gmail.com>
2024-03-24 13:22:29 -0600

    Added code view collection view, assessts, constants folder for images.

---
+++ CollectionViewHB/CollectionViewHB/Helper/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  CollectionViewHB
+//
+//  Created by Briana Bayne on 3/24/24.
+//
+
+import UIKit
+
+enum Images {
+    
+    static let copperBriana = UIImage(resource: .babyB)
+    static let crossFit = UIImage(resource: .CF)
+    static let summer = UIImage(resource: .summer)
+}
